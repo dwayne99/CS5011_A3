8f9e3e800bd264b7774a6668c426a1acf0c820b9
Dwayne Fernandes <Dwayne.Fernandes@Xoriant.Com>
2023-04-02 15:51:28 +0100

    commented agent, basuc

--- StarterCode/src/Agent.java
+++ StarterCode/src/Agent.java
@@ -1,15 +1,25 @@
 import java.util.Vector;
 
+/**
+ * Represents an agent that plays the Minesweeper game. The agent can probe cells, set flags, and recursively probe adjacent cells when a zero is found.
+ * This is inherrited by the Basic, Beginner and Intermediate agents
+ */
 public class Agent {
-    protected Game game;
-    protected int currentX;
-    protected int currentY;
+    protected Game game; // game being played
+    protected int currentX; // the X position of the agent
+    protected int currentY; // the Y position of the agent
 
-    protected boolean verbose;
+    protected boolean verbose; // used for printing the game state at various iterations
+    protected boolean loss; // check if the agent has lost or not
+    protected Vector<int[]> cellsToProbe = new Vector<>(); // list of all the cells that are needed to be probed
 
-    protected boolean loss;
-    protected Vector<int[]> cellsToProbe = new Vector<>();
-
+    /**
+     * Creates a new instance of the Agent class.
+     * @param game The Minesweeper game being played.
+     * @param startX The starting X coordinate of the agent.
+     * @param startY The starting Y coordinate of the agent.
+     * @param verbose Whether the agent should print out information during the game.
+     */
     public Agent(Game game, int startX, int startY, boolean verbose) {
         this.game = game;
         this.currentX = startX;
@@ -17,10 +27,17 @@ public class Agent {
         this.verbose = verbose;
     }
 
+    /**
+     * Prints out the current state of the game.
+     */
     public void displayGameState() {
         A3main.printBoard(game.getGameState());
     }
 
+    /**
+     * Gets the neighbouring cells of the current cell.
+     * @return A vector of int arrays representing the neighbouring cells.
+     */
     public Vector<int[]> getNeighbours() {
         Vector<int[]> neighbours = new Vector<>();
         int[][] directions = {{-1,-1}, {-1,0}, {0,-1}, {0,1}, {1,0}, {1,1}};
@@ -35,6 +52,12 @@ public class Agent {
         return neighbours;
     }
 
+    /**
+     * Gets the neighbouring cells of a specific cell.
+     * @param X The X coordinate of the cell.
+     * @param Y The Y coordinate of the cell.
+     * @return A vector of int arrays representing the neighbouring cells.
+     */
     public Vector<int[]> getNeighbours(int X, int Y) {
         Vector<int[]> neighbours = new Vector<>();
         int[][] directions = {{-1,-1}, {-1,0}, {0,-1}, {0,1}, {1,0}, {1,1}};
@@ -49,6 +72,9 @@ public class Agent {
         return neighbours;
     }
 
+    /**
+     * Adds neighbouring unprobed cells to the probe queue.
+     */
     protected void addCellstoProbeQueue() {
         Vector<int[]> neighbours = getNeighbours();
         for (int[] coord: neighbours) {
@@ -58,12 +84,24 @@ public class Agent {
         }
     }
 
+
+    /**
+     * Returns the cell state at the specified (x,y) coordinate and updates the game board accordingly.
+     * @param x the x-coordinate of the cell to probe
+     * @param y the y-coordinate of the cell to probe
+     * @return the current state of the cell at the specified (x,y) coordinate
+     */
     protected char probe(int x, int y) {
         char info = game.getCellState(x, y);
         game.updateCells(x, y, info);
         return info;
     }
 
+    /**
+     * Recursively probes all the adjacent cells to the current cell that have a state of '0'.
+     * Since all neighbours of 0 are safe
+     * @param info the state of the current cell
+     */
     protected void recursivelyProbeZeros(char info) {
         addCellstoProbeQueue();
 
@@ -78,7 +116,12 @@ public class Agent {
         }
     }
 
-
+    /**
+     * Determines whether a vector of integer arrays contains a specified array.
+     * @param vec the vector of integer arrays to search
+     * @param arr the array to search for in the vector
+     * @return true if the vector contains the specified array, false otherwise
+     */
     protected static boolean containsArray(Vector<int[]> vec, int[] arr) {
         for (int[] vecArr : vec) {
             if (java.util.Arrays.equals(vecArr, arr)) {
@@ -88,10 +131,19 @@ public class Agent {
         return false;
     }
 
+    /**
+     * Flags the specified cell on the game board.
+     * @param coord the (x,y) coordinates of the cell to flag
+     */
     protected void setFlag(int [] coord) {
         game.putFlag(coord);
     }
 
+    /**
+     * Returns a vector of all neighbouring cells that are probed of the specified unprobed cell.
+     * @param unprobedCell the (x,y) coordinates of the unprobed cell
+     * @return a vector containing the (x,y) coordinates of all the probed neighboring cells
+     */
     protected Vector<int[]> getProbedNeighbours(int[] unprobedCell) {
         Vector<int[]> probedNeighbours = new Vector<>();
         int[][] directions = {{-1,-1}, {-1,0}, {0,-1}, {0,1}, {1,0}, {1,1}};
--- StarterCode/src/BasicAgent.java
+++ StarterCode/src/BasicAgent.java
@@ -1,11 +1,31 @@
+
+/**
+ * The BasicAgent class represents a basic Minesweeper agent that plays the game
+ * by probing the top left cell and then taking actions based on the information
+ * obtained. This agent does not use any advanced techniques to improve its
+ * performance.
+ */
 public class BasicAgent extends Agent {
 
-
-
+    /**
+     * Constructs a new BasicAgent with the given game, starting coordinates, and
+     * verbose mode.
+     *
+     * @param game    the Minesweeper game to play
+     * @param x       the starting x-coordinate
+     * @param y       the starting y-coordinate
+     * @param verbose whether to display the game state after each action
+     */
     public BasicAgent(Game game, int x, int y, boolean verbose) {
         super(game,x,y, verbose);
     }
 
+
+    /**
+     * Plays the Minesweeper game using the basic strategy of probing the top left
+     * cell and then taking actions based on the information obtained. This method
+     * runs until the game ends, either through a win or a loss.
+     */
     public void playGame() {
 
         if (verbose) { // display initial state of the game
@@ -18,22 +38,26 @@ public class BasicAgent extends Agent {
             this.currentY = xyTopLeft[1];
             char info = probe(this.currentX, this.currentY);
 
-            // based on the info perform various actions
+            // Step 2: Take action based on the info and perform various actions
             if (info == 't') {
+                // game over
                 this.loss = game.getGameOutCome();
                 if (!this.loss) {
                     game.updateCells(this.currentX,this.currentY,'?');
                 }
                 break;
             } else if (info >= '1' && info <= '6') {
+                // probe the top-left
                 xyTopLeft = game.getTopLeft();
                 this.currentX = xyTopLeft[0];
                 this.currentY = xyTopLeft[1];
             } else { // if a '0' is encountered
+                // probe all the adjacent cells recursively
                 recursivelyProbeZeros(info);
             }
 
-            if (verbose & !game.hasWon()) { // display initial state of the game
+            if (verbose & !game.hasWon()) {
+                // display the state of the game
                 displayGameState();
             }
         }
@@ -46,5 +70,4 @@ public class BasicAgent extends Agent {
         game.printGameOutcome(this.loss);
 
     }
-
 }
